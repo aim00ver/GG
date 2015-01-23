d158e7914a78a4a13148348754047867c3f09df8
aim00ver <aimO4ver>
2015-01-23 18:28:41 +0200

    init

---
+++ GAI/fmMain.Designer.cs
@@ -0,0 +1,296 @@
+﻿namespace GAI
+{
+    partial class fmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.buttonGrab = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.tabBrowser = new System.Windows.Forms.TabControl();
+            this.tabPageBrowser = new System.Windows.Forms.TabPage();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.webBrowser = new System.Windows.Forms.WebBrowser();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.listView2 = new System.Windows.Forms.ListView();
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.comboBoxExam = new System.Windows.Forms.ComboBox();
+            this.numericUpDownQuestion = new System.Windows.Forms.NumericUpDown();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.tabBrowser.SuspendLayout();
+            this.tabPageBrowser.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownQuestion)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.numericUpDownQuestion);
+            this.panel1.Controls.Add(this.comboBoxExam);
+            this.panel1.Controls.Add(this.buttonGrab);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 622);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(911, 36);
+            this.panel1.TabIndex = 0;
+            // 
+            // buttonGrab
+            // 
+            this.buttonGrab.Location = new System.Drawing.Point(7, 6);
+            this.buttonGrab.Name = "buttonGrab";
+            this.buttonGrab.Size = new System.Drawing.Size(75, 23);
+            this.buttonGrab.TabIndex = 0;
+            this.buttonGrab.Text = "Grab";
+            this.buttonGrab.UseVisualStyleBackColor = true;
+            this.buttonGrab.Click += new System.EventHandler(this.buttonGrab_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.tabBrowser);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(911, 622);
+            this.panel2.TabIndex = 1;
+            // 
+            // tabBrowser
+            // 
+            this.tabBrowser.Controls.Add(this.tabPageBrowser);
+            this.tabBrowser.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabBrowser.Location = new System.Drawing.Point(0, 0);
+            this.tabBrowser.Name = "tabBrowser";
+            this.tabBrowser.SelectedIndex = 0;
+            this.tabBrowser.Size = new System.Drawing.Size(911, 622);
+            this.tabBrowser.TabIndex = 0;
+            // 
+            // tabPageBrowser
+            // 
+            this.tabPageBrowser.Controls.Add(this.splitContainer1);
+            this.tabPageBrowser.Location = new System.Drawing.Point(4, 22);
+            this.tabPageBrowser.Name = "tabPageBrowser";
+            this.tabPageBrowser.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageBrowser.Size = new System.Drawing.Size(903, 596);
+            this.tabPageBrowser.TabIndex = 1;
+            this.tabPageBrowser.Text = "Browser";
+            this.tabPageBrowser.UseVisualStyleBackColor = true;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(3, 3);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.webBrowser);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.splitContainer2);
+            this.splitContainer1.Size = new System.Drawing.Size(897, 590);
+            this.splitContainer1.SplitterDistance = 638;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // webBrowser
+            // 
+            this.webBrowser.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser.Name = "webBrowser";
+            this.webBrowser.Size = new System.Drawing.Size(638, 590);
+            this.webBrowser.TabIndex = 2;
+            this.webBrowser.Url = new System.Uri("", System.UriKind.Relative);
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            this.splitContainer2.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.listView2);
+            this.splitContainer2.Panel1.Controls.Add(this.panel3);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.listView1);
+            this.splitContainer2.Size = new System.Drawing.Size(255, 590);
+            this.splitContainer2.SplitterDistance = 396;
+            this.splitContainer2.TabIndex = 0;
+            // 
+            // listView2
+            // 
+            this.listView2.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.listView2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView2.GridLines = true;
+            this.listView2.Location = new System.Drawing.Point(0, 71);
+            this.listView2.MultiSelect = false;
+            this.listView2.Name = "listView2";
+            this.listView2.Size = new System.Drawing.Size(255, 325);
+            this.listView2.TabIndex = 2;
+            this.listView2.UseCompatibleStateImageBehavior = false;
+            this.listView2.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Exam";
+            this.columnHeader2.Width = 67;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Ticket #";
+            this.columnHeader3.Width = 83;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Questions";
+            this.columnHeader4.Width = 114;
+            // 
+            // panel3
+            // 
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(255, 71);
+            this.panel3.TabIndex = 0;
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1});
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.GridLines = true;
+            this.listView1.Location = new System.Drawing.Point(0, 0);
+            this.listView1.MultiSelect = false;
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(255, 190);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Question #";
+            this.columnHeader1.Width = 254;
+            // 
+            // comboBoxExam
+            // 
+            this.comboBoxExam.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxExam.FormattingEnabled = true;
+            this.comboBoxExam.Items.AddRange(new object[] {
+            "Kiev",
+            "Kharkov"});
+            this.comboBoxExam.Location = new System.Drawing.Point(89, 8);
+            this.comboBoxExam.Name = "comboBoxExam";
+            this.comboBoxExam.Size = new System.Drawing.Size(88, 21);
+            this.comboBoxExam.TabIndex = 1;
+            // 
+            // numericUpDownQuestion
+            // 
+            this.numericUpDownQuestion.Location = new System.Drawing.Point(183, 8);
+            this.numericUpDownQuestion.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDownQuestion.Name = "numericUpDownQuestion";
+            this.numericUpDownQuestion.Size = new System.Drawing.Size(79, 20);
+            this.numericUpDownQuestion.TabIndex = 2;
+            this.numericUpDownQuestion.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // fmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(911, 658);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "fmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "GAI Grabber";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.tabBrowser.ResumeLayout(false);
+            this.tabPageBrowser.ResumeLayout(false);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownQuestion)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TabControl tabBrowser;
+        private System.Windows.Forms.TabPage tabPageBrowser;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.ListView listView2;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.WebBrowser webBrowser;
+        private System.Windows.Forms.Button buttonGrab;
+        private System.Windows.Forms.ComboBox comboBoxExam;
+        private System.Windows.Forms.NumericUpDown numericUpDownQuestion;
+    }
+}
+
---
+++ GAI/fmMain.cs
@@ -0,0 +1,257 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GAI
+{
+    #region serializer
+    public static class ObjectCopier
+    {
+        /// <summary>
+        /// Perform a deep Copy of the object.
+        /// </summary>
+        /// <typeparam name="T">The type of object being copied.</typeparam>
+        /// <param name="source">The object instance to copy.</param>
+        /// <returns>The copied object.</returns>
+        public static T Clone<T>(T source)
+        {
+            if (!typeof(T).IsSerializable)
+            {
+                throw new ArgumentException("The type must be serializable.", "source");
+            }
+
+            // Don't serialize a null object, simply return the default for that object
+            if (Object.ReferenceEquals(source, null))
+            {
+                return default(T);
+            }
+
+            IFormatter formatter = new BinaryFormatter();
+            Stream stream = new MemoryStream();
+            using (stream)
+            {
+                formatter.Serialize(stream, source);
+                stream.Seek(0, SeekOrigin.Begin);
+                return (T)formatter.Deserialize(stream);
+            }
+        }
+
+        public static byte[] PackToBinaryBox<T>(T source)
+        {
+            if (!typeof(T).IsSerializable)
+            {
+                throw new ArgumentException("The type must be serializable.", "source");
+            }
+
+            IFormatter formatter = new BinaryFormatter();
+            Stream stream = new MemoryStream();
+            using (stream)
+            {
+                formatter.Serialize(stream, source);
+                stream.Seek(0, SeekOrigin.Begin);
+                byte[] array = new byte[stream.Length];
+                stream.Read(array, 0, (int)stream.Length);
+                return array;
+            }
+        }
+
+        public static object UnpackFromBinaryBox(byte[] box)
+        {
+            IFormatter formatter = new BinaryFormatter();
+            Stream stream = new MemoryStream();
+            using (stream)
+            {
+                stream.Write(box, 0, box.Length);
+                stream.Seek(0, SeekOrigin.Begin);
+                return formatter.Deserialize(stream);
+            }
+        }
+    }
+    #endregion
+    public partial class fmMain : Form
+    {
+        private static Exams _exams = null;
+        public static Exams ExamsList
+        {
+            get
+            {
+                if(_exams == null)
+                {
+                    _exams = Exams.Load();
+                }
+                return _exams;
+            }
+        }
+        public fmMain()
+        {
+            InitializeComponent();
+            //var t = ExamsList;
+        }
+
+        private void buttonGrab_Click(object sender, EventArgs e)
+        {
+            if (comboBoxExam.SelectedIndex == -1)
+                comboBoxExam.SelectedIndex = 0;
+            var exam = (int)Enum.Parse(typeof(Exams.Exam.ExamType), comboBoxExam.SelectedText);
+            var maxTicket = (int)Enum.Parse(typeof(Exams.Exam.ExamTicketsNumber), comboBoxExam.SelectedText);
+            numericUpDownQuestion.Maximum = maxTicket;
+            var url = String.Format("http://online.vodiy.kiev.ua/pdr/test/?complect={0}&bilet={1}&training=on&adv=on&stat=off",
+                                    exam, numericUpDownQuestion.Value);
+            try
+            {
+                webBrowser.DocumentCompleted -= webBrowser_DocumentCompleted;
+                webBrowser.Navigate(url);
+                webBrowser.DocumentCompleted += webBrowser_DocumentCompleted;
+            }
+            catch
+            {
+            }
+        }
+
+        void webBrowser_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        [Serializable]
+        public class Exams
+        {
+            [Serializable]
+            public class Exam
+            {
+                #region enums
+                public enum ExamType
+                {
+                    Kharkov = 0,
+                    Kiev = 3
+                }
+                public enum ExamTicketsNumber
+                {
+                    Kharkov = 82,
+                    Kiev = 126
+                }
+                #endregion
+                [Serializable]
+                public class Ticket
+                {
+                    [Serializable]
+                    public class Question
+                    {
+                        public int Number { get; set; }
+                        public string QuestionText { get; set; }
+                        public List<string> Options { get; set; }
+                        public int CorrectOption { get; set; }
+                        public string Answer { get; set; }
+                        public string FullHtml { get; set; }
+                        public bool IsDuplicate { get; set; }
+                        public int Hash
+                        {
+                            get
+                            {
+                                throw new NotImplementedException();
+                            }
+                        }
+                        public Question()
+                        {
+                            Options = new List<string>();
+                        }
+                    }
+                    public Ticket()
+                    {
+                        Questions = new List<Question>();
+                    }
+                    public Exam Exam { get; set; }
+                    public List<Question> Questions { get; set; }
+                }
+                public Exam()
+                {
+                    Tickets = new List<Ticket>();
+                }
+                public bool IsComplete { get; set; }
+                public List<Ticket> Tickets { get; set; }
+            }
+            public Exams()
+            {
+                ExamsList = new List<Exam>();
+            }
+            public List<Exam> ExamsList { get; set; }
+
+            #region serialization
+            public static string WorkingDir
+            {
+                get
+                {
+                    return System.IO.Path.GetDirectoryName(System.Windows.Forms.Application.ExecutablePath);
+                }
+            }
+            public void Save()
+            {
+                Save(WorkingDir + "\\exams.bin");
+            }
+            public void Save(string fileName)
+            {
+                Stream stream = null;
+                try
+                {
+                    IFormatter formatter = new BinaryFormatter();
+                    stream = new FileStream(fileName, FileMode.Create, FileAccess.Write, FileShare.None);
+                    formatter.Serialize(stream, this);
+                }
+                catch
+                {
+                    throw;
+                }
+                finally
+                {
+                    if (null != stream)
+                        stream.Close();
+                }
+            }
+            public static Exams Load()
+            {
+                return Load(WorkingDir + "\\exams.bin");
+            }
+            public static Exams Load(string fileName)
+            {
+                Exams s = null;
+                Stream stream = null;
+                try
+                {
+                    if (File.Exists(fileName))
+                    {
+                        IFormatter formatter = new BinaryFormatter();
+                        stream = new FileStream(fileName, FileMode.Open, FileAccess.Read, FileShare.None);
+                        s = (Exams)formatter.Deserialize(stream);
+                    }
+                    else
+                    {
+                        s = new Exams();
+                        IFormatter formatter = new BinaryFormatter();
+                        stream = new FileStream(fileName, FileMode.Create, FileAccess.Write, FileShare.None);
+                        formatter.Serialize(stream, s);
+                    }
+                }
+                catch
+                {
+                    throw;
+                }
+                finally
+                {
+                    if (null != stream)
+                        stream.Close();
+                }
+                return s;
+            }
+            #endregion
+        }
+    }
+}
